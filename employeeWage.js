449867827a8e4b4a7e64f2fc370a25514697ffaa
Chaithu2002 <chaithuchowdhary333@gmail.com>
2024-02-13 15:26:02 +0530

    ADDED: Employee Attendance

---
+++ employeeWage.js
@@ -0,0 +1,11 @@
+function employeeWage(){
+    // greeting's message to users
+    console("Welcome to Employe Wage Computation Program on Master Branch");
+    EmpAttendance = Math.round(Math.random()*1)
+    if(EmpAttendance == 1){
+        return "Present";
+    }else{
+        return "Absent";
+    }
+    
+}
