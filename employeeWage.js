a7ed64c5737095cb848ebc2c197e4db0eaa489ed
Chaithu2002 <chaithuchowdhary333@gmail.com>
2024-02-14 10:39:30 +0530

    ADDED: calculating wages if employee is part time

--- employeeWage.js
+++ employeeWage.js
@@ -4,10 +4,18 @@ function employeeAttendance(){
     // employee attendance
     EmpAttendance = Math.round(Math.random()*1)
     if(EmpAttendance == 1){
-        console.log("employee is present");
         let perHourWage = 20;
         let fullTimeHours  = 8;
         let totalWage = fullTimeHours * perHourWage;
+        let partTimeWage = 4 * perHourWage;
+        let partTime = Math.round(Math.random()*8);
+        // if employee is worked for part time hours
+        if(partTime == 4){
+            console.log("employee is present for only part time hours");
+            return `Total employee wage for part Time is ${partTimeWage} rupees`;
+        }
+        // if employess is present full time hours
+        console.log("employee is present for full time hours");
         return `Total wage for employee is ${totalWage} rupees`;
     }else{
         return "employee is absent";
