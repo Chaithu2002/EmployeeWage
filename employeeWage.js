ec4bd62225e54f1c84165d5828339f8f55f05888
Chaithu2002 <chaithuchowdhary333@gmail.com>
2024-02-14 14:30:43 +0530

    MODIFIED: Calculating monthly wage for employee

--- employeeWage.js
+++ employeeWage.js
@@ -1,31 +1,54 @@
-// employee wage calculating using switch statement
-
 console.log("Welcome to Employee Wage Computation Program on Master Branch");
 
-let employeeStatus = ()=>{                //using arrow function for getting employee attendance either 1 or 0;
-    return Math.round(Math.random()*1);
-}
+// calculating wages for a month
 
-switch(employeeStatus()){
-    case 1:
+function employeeWage(){
+
+    var daysInMonth = 30;
+    var day = 1;
+    var partTimeDays = 0;
+    var fullTimeDays = 0;
+    var totalMonthWage = 0;
+    
+    // while loop for calculating the employee total wages and no of days present full time or part time.
+    
+    while(day <= daysInMonth){
+        let presentDays;
         let perHourWage = 20;
         let fullTimeHours  = 8;
-        let totalWage = fullTimeHours * perHourWage;
+        let fullTimeWage = fullTimeHours * perHourWage;
         let partTimeWage = 4 * perHourWage;
-        let partTime = Math.round(Math.random()*8);
-        // if employee is worked for part time hours
-        if(partTime == 4){
-            console.log("employee is present for only part time hours");
-            console.log(`Total employee wage for part Time is ${partTimeWage} rupees`);
+        // taking attendance for 20 days
+        let empAttendance = ()=>{
+            return Math.round(Math.random()*1);
         }
-        // if employess is present full time hours
-        console.log("employee is present for full time hours");
-        console.log(`Total wage for employee is ${totalWage} rupees`);
-        break;
-    case 0:
-        console.log("employee is absent");
-        break; 
+        
+        if(empAttendance() == 1){
+            presentDays++;
+            // checking if employee has present for 20 days
+            if(presentDays == 20){
+                return totalMonthWage;
+            }
+            
+            // calculating wage for part time hours
+            let partTimeHours = Math.round(Math.random()*4);
+            if(partTimeHours == 4){
+                let wage = partTimeWage;
+                totalMonthWage += wage;
+                partTimeDays++;
+            }
+            // calculating wages for full time hours
+            else{
+                let wage = fullTimeWage;
+                totalMonthWage += wage;
+                fullTimeDays++;
+            }
+        }
+        day++;
+    }
+    console.log(`Employee has worked ${partTimeDays} part time days and ${fullTimeDays} full time days`);
+    return `Total wages is ${totalMonthWage} rupees`
 }
 
-
-
+let result = employeeWage();
+console.log(result);
